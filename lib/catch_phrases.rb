b3f9f83b0621584e28195d8ecdb765de155eeb0b
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-03 18:14:05 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,20 +1,3 @@
 def mario
   phrase = "It's-a me, Mario!"
 end
-
-def toadstool
-   status = "Thank You Mario! But Our Princess Is In Another Castle!"
- end
- 
-def link
-  statement = "It's Dangerous To Go Alone! Take This."
-  puts statement
-end
-
-def all_phrases
-  puts mario 
-  puts toadstool
-  puts link
-end
-
-all_phrases
